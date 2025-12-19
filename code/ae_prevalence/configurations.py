5c06ba709361e1955335a2ff9932a3f2765edc87
Ayush Noori <anoori@college.harvard.edu>
2025-12-19 01:00:32 -0500

    Clean up configurations

--- code/ae_prevalence/configurations.py
+++ code/ae_prevalence/configurations.py
@@ -43,30 +43,6 @@ The following keys are supported:
 """
 
 CONFIGURATIONS = [
-    {
-        "disease": "diabetes",
-        "comorbidity": "chronic kidney disease",
-        "drugs": ["dulaglutide"],
-        "aes": ["pancreatitis", "hypoglycemia"],
-        "enabled": False,
-    },
-    {
-        "disease": "hypertension",
-        "comorbidity": "chronic kidney disease",
-        "drugs": ["losartan", "valsartan", "irbesartan", "candesartan", "olmesartan"],
-        "drug_group_name": "ARB",
-        "aes": ["hyperkalemia", "systemic lupus erythematosus"],
-        "enabled": False,
-    },
-    # Success: BRASH
-    {
-        "disease": "hypertension",
-        "comorbidity": "gout",
-        "drugs": ["atenolol", "metoprolol", "propranolol", "bisoprolol", "carvedilol", "labetalol", "timolol", "oxprenolol", "pindolol"],
-        "drug_group_name": "beta-blocker",
-        "aes": ["acute kidney failure", "unspecified acute kidney failure", "hyperkalemia", "cardiac dysrhythmia"],
-        "enabled": True,
-    },
     # Success
     {
         "disease": "hypertension",
@@ -77,14 +53,25 @@ CONFIGURATIONS = [
         "negative_controls": ["corneal abrasion", "gingivitis", "insect bite"],
         "enabled": True,
     },
-    # Failure
+    # Success: BRASH
     {
         "disease": "hypertension",
         "comorbidity": "gout",
-        "drugs": ["valsartan", "irbesartan", "candesartan", "olmesartan"],
-        "drug_group_name": "non-losartan ARB",
-        "aes": ["systemic lupus erythematosus"],
-        "enabled": False,
+        "drugs": ["atenolol", "metoprolol", "propranolol", "bisoprolol", "carvedilol", "labetalol", "timolol", "oxprenolol", "pindolol"],
+        "drug_group_name": "beta-blocker",
+        "aes": ["acute kidney failure", "unspecified acute kidney failure", "hyperkalemia", "cardiac dysrhythmia"],
+        "negative_controls": ["corneal abrasion", "gingivitis", "insect bite"],
+        "enabled": True,
+    },
+    # Success
+    {
+        "disease": "hyperlipidemia",
+        "comorbidity": "hypothyroidism",
+        "drugs": ["atorvastatin", "simvastatin", "rosuvastatin", "pravastatin", "lovastatin", "fluvastatin", "cerivastatin"],
+        "drug_group_name": "statin",
+        "aes": ["liver failure"],
+        "negative_controls": ["corneal abrasion", "gingivitis", "insect bite"],
+        "enabled": True,
     },
     # Success
     {
@@ -94,14 +81,6 @@ CONFIGURATIONS = [
         "drug_group_name": "DPP-4 inhibitor",
         "aes": ["hepatocellular carcinoma"],
         "enabled": True,
-    },
-    # Failure: tinidazole is used to treat bacterial vaginosis
-    {
-        "disease": "vaginosis",
-        "comorbidity": "pregnancy",
-        "drugs": ["tinidazole"],
-        "aes": ["neutropenia"],
-        "enabled": False,
     },
     # Success
     {
@@ -121,43 +100,11 @@ CONFIGURATIONS = [
         "aes": ["acute pancreatitis", "respiratory failure"],
         "enabled": True,
     },
-    # Failure
-    {
-        "disease": "diabetes",
-        "comorbidity": "surgical follow up",
-        "drugs": ["empagliflozin", "dapagliflozin", "ertugliflozin"],
-        "drug_group_name": "SGLT-2 inhibitor",
-        "aes": ["muscle weakness"],
-        "enabled": False,
-    },
-    # Failure (OR success, prevalence failure)
-    {
-        "disease": "hypertension",
-        "comorbidity": "chronic kidney disease",
-        "drugs": ["captopril", "enalapril", "ramipril", "benazepril", "fosinopril", "cilazapril"],
-        "drug_group_name": "ACE inhibitor",
-        # "aes": ["taste and smell alteration", "muscle weakness"],
-        "aes": ["pancreatitis", "ataxia"],
-        "enabled": False,
-    },
-    # Success
-    {
-        "disease": "hyperlipidemia",
-        "comorbidity": "hypothyroidism",
-        "drugs": ["atorvastatin", "simvastatin", "rosuvastatin", "pravastatin", "lovastatin", "fluvastatin", "cerivastatin"],
-        "drug_group_name": "statin",
-        "aes": ["liver failure"],
-        "enabled": True,
-    },
-    # Positive control: failure
-    {
-        "disease": "diabetes",
-        "comorbidity": "surgical follow up",
-        "drugs": ["empagliflozin", "dapagliflozin", "ertugliflozin"],
-        "drug_group_name": "SGLT-2 inhibitor",
-        "aes": ["urinary tract infection"],
-        "enabled": False,
-    },
+
+
+
+    # POSITIVE CONTROLS
+
     # Positive control: success
     {
         "disease": "hypertension",
@@ -193,24 +140,11 @@ CONFIGURATIONS = [
         "aes": ["angina"],
         "enabled": True,
     },
-    # Negative control: failure
-    {
-        "disease": "bronchial_asthma",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["atenolol", "metoprolol", "bisoprolol"],
-        "drug_group_name": "cardioselective beta-blocker",
-        "aes": ["respiratory failure"],
-        "enabled": False,
-    },
-    # Negative control: failure (prevalence success, OR failure)
-    {
-        "disease": "hypertension",
-        "comorbidity": "chronic kidney disease",
-        "drugs": ["captopril", "enalapril", "ramipril", "benazepril", "fosinopril", "cilazapril"],
-        "drug_group_name": "ACE inhibitor",
-        "aes": ["influenza"],
-        "enabled": False,
-    },
+
+
+    
+    # NEGATIVE CONTROLS
+
     # Negative control: succcess
     # https://chatgpt.com/share/691359d5-f740-8012-ae44-4de58a433dd3
     # https://www.acc.org/Latest-in-Cardiology/Clinical-Trials/2015/09/17/10/11/EMPA-REG-OUTCOME#:~:text=Secondary%20outcomes%3A
@@ -220,36 +154,6 @@ CONFIGURATIONS = [
         "drugs": ["empagliflozin", "dapagliflozin", "ertugliflozin"],
         "drug_group_name": "SGLT-2 inhibitor",
         "aes": ["acute kidney failure", "heart failure"],
-        "enabled": False,
-    },
-    # Negative control: failure (confounded by disease severity)
-    # https://chatgpt.com/share/691359d5-f740-8012-ae44-4de58a433dd3
-    {
-        "disease": "hyperlipidemia",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["atorvastatin", "simvastatin", "rosuvastatin", "pravastatin", "lovastatin", "fluvastatin", "cerivastatin"],
-        "drug_group_name": "statin",
-        "aes": ["stroke", "myocardial infarction"],
-        "enabled": False,
-    },
-    # Negative control: failure (confounded by disease severity)
-    # https://chatgpt.com/share/691359d5-f740-8012-ae44-4de58a433dd3
-    {
-        "disease": "hypertension",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["atenolol", "metoprolol", "propranolol", "bisoprolol", "carvedilol", "labetalol", "timolol", "oxprenolol", "pindolol"],
-        "drug_group_name": "beta-blocker",
-        "aes": ["heart failure", "myocardial infarction"],
-        "enabled": False,
-    },
-    # Negative control: failure
-    # https://chatgpt.com/share/691359d5-f740-8012-ae44-4de58a433dd3
-    {
-        "disease": "diabetes",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["captopril", "enalapril", "ramipril", "benazepril", "fosinopril", "cilazapril"],
-        "drug_group_name": "ACE inhibitor",
-        "aes": ["stroke", "myocardial infarction"],
         "enabled": False,
     },
     # Negative control: success
@@ -262,53 +166,5 @@ CONFIGURATIONS = [
         "drug_group_name": "GLP-1 receptor agonist",
         "aes": ["stroke", "myocardial infarction", "heart failure"],
         "enabled": True,
-    },
-    # Negative control: failure
-    {
-        "disease": "bipolar_disorder",
-        "comorbidity": "hypothyroidism",
-        "drugs": ["lithium"],
-        "aes": ["suicide attempt"],
-        "enabled": False,
-    },
-    # Negative control: failure
-    # https://chatgpt.com/c/6912a3c7-e284-8331-b7de-2b01e313921e
-    {
-        "disease": "hypertension",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["atenolol", "metoprolol", "propranolol", "bisoprolol", "carvedilol", "labetalol", "timolol", "oxprenolol", "pindolol"],
-        "drug_group_name": "beta-blocker",
-        "aes": ["migraine", "essential tremor"],
-        "enabled": False,
-    },
-    # Negative control: failure
-    # https://chatgpt.com/c/6912a3c7-e284-8331-b7de-2b01e313921e
-    {
-        "disease": "hypertension",
-        "comorbidity": "chronic kidney disease",
-        "drugs": ["captopril", "enalapril", "ramipril", "benazepril", "fosinopril", "cilazapril"],
-        "drug_group_name": "ACE inhibitor",
-        "aes": ["migraine"],
-        "enabled": False,
-    },
-    # Negative control: failure
-    # https://chatgpt.com/c/6912a3c7-e284-8331-b7de-2b01e313921e
-    {
-        "disease": "hyperlipidemia",
-        "comorbidity": "ischemic heart disease",
-        "drugs": ["atorvastatin", "simvastatin", "rosuvastatin", "pravastatin", "lovastatin", "fluvastatin", "cerivastatin"],
-        "drug_group_name": "statin",
-        "aes": ["cholelithiasis"],
-        "enabled": False,
-    },
-
-    # Negative control, unrelated phenotype: 
-    {
-        "disease": "hypertension",
-        "comorbidity": "gout",
-        "drugs": ["hydrochlorothiazide", "indapamide", "furosemide", "spironolactone", "eplerenone", "amiloride", "triamterene"],
-        "drug_group_name": "diuretic",
-        "aes": ["migraine"],
-        "enabled": False,
-    },
+    }
 ]
